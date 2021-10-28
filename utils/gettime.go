69c4f0908235f710f86193c557955f0fd78ab317
guonh <guoneihui@126.com>
2021-10-28 15:18:58 +0800

    update time

--- utils/gettime.go
+++ utils/gettime.go
@@ -8,6 +8,11 @@ func GetParseTime(alertTime string) string {
 	if err != nil {
 		return err.Error()
 	}
+	// 设置时区
+	cstSH, _ := time.LoadLocation("Asia/Shanghai")
+	// if err != nil {
+	// 	return err.Error()
+	// }
 
-	return ts.In(&time.Location{}).Format("2006-01-02 15:04:05")
+	return ts.In(cstSH).Format("2006-01-02 15:04:05")
 }
