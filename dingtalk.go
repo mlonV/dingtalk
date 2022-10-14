be18762e74ef28f426485c4451cb5118eff5f8f8
guonh <guoneihui@aoyou.com>
2022-10-14 11:23:18 +0800

    update isopen code

--- dingtalk.go
+++ dingtalk.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+
 	"github.com/mlonV/dingtalk/config"
 	der "github.com/mlonV/dingtalk/monitor/docker"
 	"github.com/mlonV/dingtalk/monitor/elk"
@@ -15,11 +17,13 @@ func main() {
 
 	//es 的日志告警
 	if config.Conf.ESAlarm.IsOpen {
+		log.Println("开启elk 功能 ，isopen: ", config.Conf.ESAlarm.IsOpen)
 		go elk.Ticker()
 	}
 
 	// 启动监控docker 内进程的监控
 	if config.Conf.MonitorDocker.IsOpen {
+		log.Println("开启docker Monitor 功能 ，isopen: ", config.Conf.ESAlarm.IsOpen)
 		go der.Ticker()
 	}
 
